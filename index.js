c5d69273ed81638af2f7fbc0d03a176ff0c93c5d
venomiele <atrandafir35@yahoo.com>
2022-01-04 03:43:43 -0800

    added try catch block

--- index.js
+++ index.js
@@ -5,21 +5,39 @@ let degreeEl = document.querySelector(".degree");
 let descEl = document.querySelector(".description");
 let humEl = document.getElementById("humidity");
 let windEl = document.getElementById("windspeed");
+let btn =  document.querySelector(".submit");
+let search =  document.querySelector(".search");
 
 async function fetchWeather (city) {
-    const response = await fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + apiKey);
-    const data =  await response.json();
-    console.log(data);
-    displayWeather(data);
+    try {
+        const response = await fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + apiKey);
+        const data =  await response.json();
+        displayWeather(data);
+    } catch {
+        alert("Something went wrong,please refresh the page!")
+    }
+
 }
-fetchWeather("tokyo");
 
 function displayWeather(data) {
 const { name } = data;
 const {icon, description} = data.weather[0];
 const {temp, humidity} = data.main;
 const {speed} = data.wind;
+cityEl.innerText = "Weather in" + " " + name;
+iconEl.src = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+descEl.innerText = description;
+degreeEl.innerText = Math.round(temp) + "°C";
+humEl.innerText = "Humidity:" + " " + humidity + "%";
+windEl.innerText = "Windspeed:" + " " + speed + "km/h";
 }
+
+btn.addEventListener("click", (e) => {
+e.preventDefault();
+result = search.value;
+fetchWeather(result);
+search.value = "";
+})
 
 
 // window.addEventListener("load", () => {
